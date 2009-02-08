4d1432c9e6bff13ed4c034d8f349370ac7fa014b
Julian Fondren <ayrnieu@gmail.com>
2009-02-07 18:32:30 -0600

    Added docstrings.

--- src/act/networking.clj
+++ src/act/networking.clj
@@ -4,10 +4,14 @@
            (java.net InetSocketAddress InetAddress)
            (java.nio ByteBuffer)))
 
-(defn localhost []
+(defn localhost
+  "Returns the InetAddress of 127.0.0.1 rather than InetAddress/getLocalHost"
+  []
   (InetAddress/getByAddress (into-array Byte/TYPE (map byte [127 0 0 1]))))
 
 (defn create-server
+  "Binds a server socket and sets it up for nonblocking operation,
+returning a map with :port , :address , and other infrastructural fields."
   ([port] (create-server port (localhost)))
   ([port address]
     (let [sel (.openSelector (SelectorProvider/provider))
@@ -19,11 +23,15 @@
         {:sel sel, :chan chan, :addr addr, :accept accept-key,
          :port port, :address address}))))
 
-(defn close-server [server]
+(defn close-server
+  "Closes a server."
+  [server]
   (.close (:chan server))
   (.close (:sel server)))
 
-(defn iterator-seq! [#^java.util.Iterator iter]
+(defn- iterator-seq!
+  "A variant of iterator-seq that leaves the iterator empty."
+  [#^java.util.Iterator iter]
   (loop [acc ()]
     (if (.hasNext iter)
       (let [e (.next iter)]
@@ -31,26 +39,44 @@
         (recur (conj acc e)))
       (reverse acc))))
 
-(defn select [server] (-> server :accept .selector .select))
-(defn selected [server] (-> server :sel .selectedKeys .iterator iterator-seq!))
+(defn select
+  "Waits for activity on a server."
+  [server]
+  (-> server :accept .selector .select))
 
-(defn select-type [#^SelectionKey x]
+(defn selected
+  "Returns a list of SelectionKeys of server activity."
+  [server]
+  (-> server :sel .selectedKeys .iterator iterator-seq!))
+
+(defn select-type
+  "Maps SelectionKeys to one of (:accept :read :write)"
+  [#^SelectionKey x]
   (cond (.isAcceptable x) :accept
         (.isReadable x) :read
         (.isWritable x) :write
         true (throw (Exception. (str "unknown accept-type: " x)))))
 
-(defn push-readable [server #^SelectionKey x]
+(defn push-readable
+  "Adds a SelectionKey to a server, so that it will receive read events."
+  [server #^SelectionKey x]
   (let [#^SocketChannel channel (.accept (.channel x))]
     (.configureBlocking channel false)
     (let [#^SelectionKey read-key
           (.register channel (:sel server) SelectionKey/OP_READ)]
       read-key)))
 
-(defn pop-readable [server #^SelectionKey k]
+(defn pop-readable
+  "Registers a SelectionKey for removal from a server."
+  [server #^SelectionKey k]
   (.cancel k))
 
-(defn socket-recv [#^SelectionKey key #^ByteBuffer buf]
+(defn socket-recv
+  "Fill a ByteBuffer from a socket."
+  [#^SelectionKey key #^ByteBuffer buf]
   (apply str (map char (take (.read (.channel key) buf) (.array buf)))))
-(defn socket-send [#^SelectionKey key #^String s]
+
+(defn socket-send
+  "Write a String to a socket."
+  [#^SelectionKey key #^String s]
   (.write (.channel key) (ByteBuffer/wrap (.getBytes s))))
